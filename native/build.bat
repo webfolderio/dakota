d4900b23950146aabf3e7c157dca13d5f4fbc2f7
webfolderio <support@webfolder.io>
2018-11-12 11:17:43 +0300

    Removed unused code

--- native/build.bat
+++ native/build.bat
@@ -1,6 +1,7 @@
+cd ..
 if not exist .\build mkdir build
 cd build
-cmake .. ^
+cmake ..\native ^
  -G"Visual Studio 15 2017 Win64" ^
  -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake ^
  -DVCPKG_TARGET_TRIPLET=x64-windows-static
