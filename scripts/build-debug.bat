8d97c7e0e1c1b64dfcb6c9ffae4bf2595c628f62
webfolderio <support@webfolder.io>
2019-02-13 16:19:00 +0300

    fixed

--- scripts/build-debug.bat
+++ scripts/build-debug.bat
@@ -5,6 +5,6 @@ if not exist .\build-debug mkdir build-debug
 cd build-debug
 cmake .. ^
  -G"Visual Studio 15 2017 Win64" ^
- -DCMAKE_TOOLCHAIN_FILE=C:\vcpkg\scripts\buildsystems\vcpkg.cmake ^
+ -DCMAKE_TOOLCHAIN_FILE=C:\tools\vcpkg\scripts\buildsystems\vcpkg.cmake ^
  -DVCPKG_TARGET_TRIPLET=x64-windows-static
 cmake --build . --config RelWithDebInfo
