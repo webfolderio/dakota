0ce4075d3c1e2a91ed5c2ef88db1c0f8cd38c75c
WebFolder OÜ <support@webfolder.io>
2019-02-14 16:33:04 +0300

    Update build.bat

--- scripts/build.bat
+++ scripts/build.bat
@@ -5,7 +5,7 @@ cd native
 if not exist .\build mkdir build
 cd build
 cmake .. ^
- -G"Visual Studio 14 2015 Win64" ^
+ -G"Visual Studio 15 2017 Win64" ^
  -DCMAKE_TOOLCHAIN_FILE=C:/Tools/vcpkg/scripts/buildsystems/vcpkg.cmake ^
  -DVCPKG_TARGET_TRIPLET=x64-windows-static
 cmake --build . --target dakota --config Release
