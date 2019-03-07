3fd5468fd1d58eadcbad3a32ead09cc9ebde5caa
webfolderio <support@webfolder.io>
2019-03-07 16:34:12 +0300

    Fixed

--- scripts/build-debug.bat
+++ scripts/build-debug.bat
@@ -1,7 +1,7 @@
 set curdir=%cd%
 cd /D "%~dp0"
-"C:\Program Files\git\usr\bin\patch.exe" "C:\tools\vcpkg\installed\x64-windows-static\include\restinio\impl\connection.hpp" "native\connection-counter.patch"
 cd ..
+"C:\Program Files\git\usr\bin\patch.exe" "C:\tools\vcpkg\installed\x64-windows-static\include\restinio\impl\connection.hpp" "native\connection-counter.patch"
 cd native
 if not exist .\build mkdir build
 cd build
