f164cc1b44fb29730243a2016b423c69107b187d
webfolderio <support@webfolder.io>
2019-03-08 19:07:37 +0300

    Prefer to use free port instead of static one

--- src/test/java/io/webfolder/dakota/TestQueryParam.java
+++ src/test/java/io/webfolder/dakota/TestQueryParam.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.Map;
 
 import org.junit.After;
@@ -20,11 +21,21 @@ public class TestQueryParam {
 
     private WebServer server;
 
+    private int freePort;
+
     private Map<String, String> queryMap;
 
     @Before
     public void init() {
-        server = new WebServer();
+        try (ServerSocket socket = new ServerSocket(0)) {
+            this.freePort = socket.getLocalPort();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Settings settings = new Settings(freePort);
+
+        server = new WebServer(settings);
 
         Request request = server.getRequest();
         Response response = server.getResponse();
@@ -52,7 +63,7 @@ public class TestQueryParam {
     public void test() throws IOException {
         OkHttpClient client = new Builder().writeTimeout(240, SECONDS).readTimeout(240, SECONDS)
                 .connectTimeout(240, SECONDS).build();
-        okhttp3.Request req = new okhttp3.Request.Builder().url("http://localhost:8080/foo?foo=bar&abc=123").build();
+        okhttp3.Request req = new okhttp3.Request.Builder().url("http://localhost:" + freePort + "/foo?foo=bar&abc=123").build();
         client.newCall(req).execute();
         assertNotNull(queryMap);
         assertEquals(2, queryMap.size());
